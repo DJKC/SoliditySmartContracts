d854e9017b4b391ed081272b3edc5ee2bb11fe29
K.Coulter <lasjapas@yahoo.com>
2022-10-07 07:01:55 +0000

    Create RealEstate.js
    Real Estate js test file from https://www.youtube.com/watch?v=eoQJ6nFZOcs&t=5466s.
    I got tired of copy and pasting since he did not explain the finer points and I was not learning much.

---
+++ RealEstate.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("RealEstate", () => {
+	let realEstate, escrow
+	let deployer, seller
+	let nftID = 1
+
+	beforeEach(async () => {
+		// set up accounts
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
+		seller   = deployer
+		buyer    = accounts[1]
+
+		// load ethers contract
+		const RealEstate = await ethers.getContractFactory("RealEstate")
+		const Escrow     = await ethers.getContractFactory("Escrow")
+
+		// deploy contract on blockchain
+		realEstate = await RealEstate.deploy()
+		escrow     = await Escrow.deploy(realEstate.address, nftID, seller, buyer)
+	})
+
+	describe("Deployment", async () => {
+		it("sends nft to seller / deployer", async() => {
+			expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
+		})
+	})
+
+	describe("Selling Real Estate", async () => {
+		// expects seller to be NFT owner before the sale
+		it("executes successful transaction", async() => {
+			expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
+		})
+	})
+})
